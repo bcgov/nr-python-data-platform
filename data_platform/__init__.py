a44a48ca0e8b59a688e3c866cfd52ca99bb42eb9
Ting Ouyang <ting.ouyang@gov.bc.ca>
2026-01-22 13:52:07 -0800

    Rename logging module to app_logging in __init__.py

--- data_platform/__init__.py
+++ data_platform/__init__.py
@@ -2,4 +2,4 @@
 # data_platform/__init__.py
 from .db.db_connection import get_connection
 from .db.executor import run_query
-from .logging.logger import setup_logger
+from .app_logging.logger import setup_logger
